9f477c3854ef17478c282db87fcedd0228a3b354
Bishal Adhikari <Vishaladhikari911@gmail.com>
2024-04-24 14:02:36 +0545

    foundatoin

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const apiKey = 'FVqReQUHGBzu36QQdfmzINPK9WwXCmgN9ISRpJM3';
-const URL = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=1&tags=BASH,DevOps,Docker,HTML,JavaScript,Kubernetes,Laravel,Linux,MySQL,PHP,Python,WordPress`;
-const anotherurl = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=1`;
-const catagoryURL = `https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=linux&limit=1`;
-const catagoryanddiffcultyurl = `https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=linux&difficulty=Easy&limit=1`;
+const URL = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=1`;
+let catagory = '';
+let difficulty = '';
 
 
-async function fetchData(){
-	const res = await fetch(anotherurl);
-	const result = res.json();
+const data = async ()=>{
+    
 
-	console.log(result);
+    try{
+        let response = await(fetch(URL));
+        let res = await response.json();
+        let question = res[1];
+        console.log(question);
+    }catch(e){
+        console.log("something happened" , e);
+    }
+    
 
 }
 
-//This is btn
-const submitBtn = document.querySelector('.submit-btn');
 
-submitBtn.addEventListener('click', ()=>{fetchData()});
+data();
+
+
