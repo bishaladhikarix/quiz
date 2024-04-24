6c90c42feb161e2ad74b63e547be10fbf91492db
Bishal Adhikari <Vishaladhikari911@gmail.com>
2024-04-24 23:02:17 +0545

    tried to check the answer

--- Client/main.js
+++ Client/main.js
@@ -3,12 +3,17 @@ const questionContainer = document.querySelector('.question');
 const ansContainer = document.querySelector('.answer');
 
 
-
 //Api's shits
 const apiKey = 'FVqReQUHGBzu36QQdfmzINPK9WwXCmgN9ISRpJM3';
 const URL = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=1`;
 let catagory = '';
 let difficulty = '';
+
+//Answer checking varaibles
+let firstAns = false;
+let secondAns = false;
+let thirdAns = false;
+let fourthAns = false;
 
 
 //assign question to questoin field
@@ -34,13 +39,88 @@ const assignAnswers = (answers)=>{
     optionThree.innerText = 'C) ' + optionC;
     optionFour.innerText = 'D) ' + optionD;
 }
+//function to check the answer whether it is correct or incorrect;
+const checkAnswers = (ans)=>{
 
-//takes responsibility for passing the data to related functions;
-const assignValues = (question,answers)=>{
-    assignQuestion(question);
-    assignAnswers(answers);
 }
 
+//takes responsibility for passing the data to related functions;
+const assignValues = (question,answers,correctAns)=>{
+    assignQuestion(question);
+    assignAnswers(answers);
+    checkAnswers(correctAns);
+}
+
+const checkAnswer = ()=>{
+    if(firstAns){
+        console.log("first answer has been changed");
+
+    }
+    if(secondAns){
+        console.log("second answer has been changed");
+
+    }
+    if(thirdAns){
+        console.log("thrid answer has been changed");
+
+    }
+    if(fourthAns){
+        console.log("fourht answer has been changed");
+    }
+}
+
+const click = ()=>{
+
+    const firstOption = document.querySelector('.option-a');
+    const secondOption = document.querySelector('.option-a');
+    const thirdOption = document.querySelector('.option-a');
+    const fourthOption = document.querySelector('.option-a');
+
+    firstOption.addEventListener('click',()=>{
+        if(firstOption.style.backgroundColor === 'blue'){
+            firstOption.style.backgroundColor = 'aliceblue'
+            firstAns = !firstAns;
+        }else{
+            firstOption.style.backgroundColor = 'blue';
+            firstAns = !firstAns;
+        }
+    })
+
+    secondOption.addEventListener('click',()=>{
+        if(secondOption.style.backgroundColor === 'blue'){
+            secondOption.style.backgroundColor = 'aliceblue'
+            secondAns = !secondAns;
+        }else{
+            secondOption.style.backgroundColor = 'blue';
+            secondAns = !secondAns;
+        }
+    })
+
+    thirdOption.addEventListener('click',()=>{
+        if(thirdOption.style.backgroundColor === 'blue'){
+            thirdOption.style.backgroundColor = 'aliceblue'
+            thirdAns = !thirdAns;
+        }else{
+            thirdOption.style.backgroundColor = 'blue';
+            thirdAns = !thirdAns;
+        }
+    })
+
+    fourthOption.addEventListener('click',()=>{
+        if(fourthOption.style.backgroundColor === 'blue'){
+            fourthOption.style.backgroundColor = 'aliceblue'
+            fourthAns = !fourthAns;
+        }else{
+            fourthOption.style.backgroundColor = 'blue';
+            fourthAns = !fourthAns;
+        }
+    })
+    
+    
+    
+    
+    
+}
 
 const data = async ()=>{
     
@@ -48,22 +128,24 @@ const data = async ()=>{
     try{
         let response = await(fetch(URL));
         let res = await response.json();
-        question = res[0].question;
+        let question = res[0].question;
         let answers = res[0].answers;
         let multipleCorrectAns = res[0].multiple_correct_answers;
         let correctAns = res[0].correct_answer;
         
-        assignValues(question,answers);
+        assignValues(question,answers,correctAns);
         
 
     }catch(e){
-        console.log("something happened" , e);
+        console.log("Unable to fetch the data due to some unfotunate circumstances" , e);
     }
     
 
 }
+document.addEventListener('DOMContentLoaded', () => {
+    click();
+});
 
 
 data();
-
 
