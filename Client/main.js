4964a8e0ef16c841be0588f44d1caa9c4ee795ce
Bishal Adhikari <Vishaladhikari911@gmail.com>
2024-04-24 14:20:19 +0545

    added question in question div

--- Client/main.js
+++ Client/main.js
@@ -1,7 +1,19 @@
+//frontend varaibles
+const questionContainer = document.querySelector('.question');
+const ansContainer = document.querySelector('.answer');
+
+
+
+//Api's shits
 const apiKey = 'FVqReQUHGBzu36QQdfmzINPK9WwXCmgN9ISRpJM3';
 const URL = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=1`;
 let catagory = '';
 let difficulty = '';
+
+
+const assignValues = (question,correctAns)=>{
+    questionContainer.innerText = question;
+}
 
 
 const data = async ()=>{
@@ -10,8 +22,13 @@ const data = async ()=>{
     try{
         let response = await(fetch(URL));
         let res = await response.json();
-        let question = res[1];
-        console.log(question);
+        question = res[0].question;
+        let answers = res[0].answers;
+        let multipleCorrectAns = res[0].multiple_correct_answers;
+        let correctAns = res[0].correct_answer;
+        
+        assignValues(question,correctAns);
+
     }catch(e){
         console.log("something happened" , e);
     }
